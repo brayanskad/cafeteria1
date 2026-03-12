c4c31e10d6c1a0c2551151a27283873a21ae0f45
brayanskad <marquesbrayan4002@gmail.com>
2026-03-12 16:57:27 -0500

    document

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const lista = document.querySelector('.lista-produtos ');
+const lista = document.querySelector(".lista-produtos");
 
 const cafe = [
     {
@@ -21,7 +21,7 @@ const cafe = [
     }
 ]
 
-const bolo = [
+const reposteria = [
     {
         nome: "Pastel de Chocolate",
         descricao: "Delicioso pastel de chocolate com recheio cremoso",
@@ -47,6 +47,54 @@ const comida = [
         nome: "Sándwich de Pavo",
         descricao: "Sándwich fresco con pavo y vegetales",
         preco: "$6,50",
-        img: "./assets/produtos/comida/sandwich.jpg"
+        img: "./assets/produtos/Comida/sandwich.jpg"
+    },
+    {
+        nome: "Ensalada César",
+        descricao: "Ensalada clásica con aderezo César",
+        preco: "$5,00",
+        img: "./assets/produtos/Comida/download.jpg"
+    },
+    {
+        nome: "Quiche de Espinacas",
+        descricao: "Quiche sabroso con espinacas y queso",
+        preco: "$7,00",
+        img: "./assets/produtos/Comida/download(1).jpg"
     }
 ]
+
+function mostrarProdutos(produtos) {
+    lista.innerHTML = "";
+    produtos.forEach(produto => {
+        lista.innerHTML += `
+                    <div class="card-popular">
+
+                        <div class="caixa-img-cards"><img class="img-cards" src="${produto.img}"
+                                alt="">
+                        </div>
+                        <div class="caixa-texto-card">
+
+                            <p class="titulo-card">${produto.nome}</p>
+                            <p class="subtitulo-card">${produto.descricao}</p>
+                            <div class="preco-btn-card">
+                                <p class="preco-card">${produto.preco}</p>
+                                <button class="btn-card"> <i class="bi bi-cart2 card-btn"></i> Agregar</button>
+                            </div>
+
+                        </div>
+
+                    </div>
+                
+   `
+    }
+    )
+}
+
+document.querySelector(".btn-cafe").addEventListener("click", () => mostrarProdutos(cafe));
+
+document.querySelector(".btn-reposteria").addEventListener("click", () => mostrarProdutos(reposteria));
+
+document.querySelector(".btn-comida").addEventListener("click", () => mostrarProdutos(comida));
+
+// produtos inciais
+mostrarProdutos(cafe);
