6ea1834c30dac7121b0968a3d1ff64e5cee04c6f
brayanskad <marquesbrayan4002@gmail.com>
2026-03-12 15:29:43 -0500

    consts

---
+++ script.js
@@ -0,0 +1,52 @@
+const lista = document.querySelector('.lista-produtos ');
+
+const cafe = [
+    {
+        nome: "expresso",
+        descricao: "Café concentrado y aromático",
+        preco: "$3,50",
+        img: "./assets/produtos/café/expresso.jpg"
+    },
+    {
+        nome: "cappuccino",
+        descricao: "Espresso con leche vaporizada y espuma",
+        preco: "$4,50",
+        img: "./assets/produtos/café/cappuccino.jpg"
+    },
+    {
+        nome: "cafe helado",
+        descricao: "Refrescante café frío con hielo",
+        preco: "$5,00",
+        img: "./assets/produtos/café/Café Helado.jpg"
+    }
+]
+
+const bolo = [
+    {
+        nome: "Pastel de Chocolate",
+        descricao: "Delicioso pastel de chocolate com recheio cremoso",
+        preco: "$4,00",
+        img: "./assets/produtos/reposteria/pastel de chocolate.jpg"
+    },
+    {
+        nome: "Croissant",
+        descricao: "Croissant francés recién horneado",
+        preco: "$3,00",
+        img: "./assets/produtos/reposteria/croissant.jpg"
+    },
+    {
+        nome: "Muffin de Arándanos",
+        descricao: "Esponjoso muffin con arándanos frescos",
+        preco: "$3,50",
+        img: "./assets/produtos/reposteria/muffin de arandanos.jpg"
+    }
+]
+
+const comida = [
+    {
+        nome: "Sándwich de Pavo",
+        descricao: "Sándwich fresco con pavo y vegetales",
+        preco: "$6,50",
+        img: "./assets/produtos/comida/sandwich.jpg"
+    }
+]
